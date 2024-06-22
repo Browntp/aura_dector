7794cec7a35bd884a11a83eb7c86fcdb0c6c4064
Browntp <130326183+Browntp@users.noreply.github.com>
2024-06-22 16:33:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.layers import TextVectorization
@@ -6,6 +6,7 @@ import numpy as np
 import pickle
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Change this to a random secret key
 
 # Load the vectorizer configuration and weights
 with open('vectorizer.pkl', 'rb') as f:
@@ -19,11 +20,9 @@ vectorizer.set_weights(vectorizer_weights)
 # Load the pre-trained model
 model = load_model('emotions_model.h5')
 
-# List to store all predictions
-predictions = []
-
 @app.route('/')
 def home():
+    predictions = session.get('predictions', [])
     total = sum(predictions)
     return render_template('index.html', predictions=predictions, total=total)
 
@@ -31,20 +30,23 @@ def home():
 def predict():
     text = request.form['text']
     # Preprocess the text input as needed
-    # Assuming input text is preprocessed properly
     input_text = vectorizer(np.array([text]))
     array = model.predict(input_text)
     aura = converter(array)
-    predictions.append(aura)
+    
+    # Store the prediction in the session
+    if 'predictions' not in session:
+        session['predictions'] = []
+    session['predictions'].append(aura)
+    session.modified = True
+    
     return redirect(url_for('home'))
-
 
 def converter(array):
     """
     # array(['anger', 'disgust', 'fear', 'guilt', 'joy', 'sadness', 'shame'],
       dtype=object)
     """
-   
     anger = array[0][0]
     disgust = array[0][1]
     fear = array[0][2]
@@ -53,8 +55,7 @@ def converter(array):
     sadness = array[0][5]
     shame = array[0][6]
 
-    return (joy + sadness + disgust - (anger + fear + guilt + shame)) * 1000 
-    #return f"{anger} {disgust} {fear} {guilt} {joy} {sadness} {shame}"
+    return (joy + sadness + disgust - (anger + fear + guilt + shame)) * 1000
 
 if __name__ == '__main__':
     app.run(debug=True)
